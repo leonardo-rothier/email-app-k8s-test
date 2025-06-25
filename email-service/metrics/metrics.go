fd1bcaee3830c5edb95959177ab26a87f9b13dbc
leonardo-rothier <leleorothier@gmail.com>
2025-06-25 02:25:42 +0000

    chore: remove unused metrics

--- email-service/metrics/metrics.go
+++ email-service/metrics/metrics.go
@@ -15,15 +15,4 @@ var (
 		Name: "app_email_errors_total",
 		Help: "Total number of email sending errors",
 	}, []string{"sender"})
-
-	HTTPRequests = promauto.NewCounterVec(prometheus.CounterOpts{
-		Name: "app_http_requests_total",
-		Help: "Total number of HTTP requests",
-	}, []string{"method", "sender", "status"})
-
-	HTTPRequestDuration = promauto.NewHistogramVec(prometheus.HistogramOpts{
-		Name:    "app_http_request_duration_seconds",
-		Help:    "Duration of HTTP requests",
-		Buckets: []float64{0.1, 0.5, 1, 2.5, 5, 10},
-	}, []string{"method", "sender"})
 )
